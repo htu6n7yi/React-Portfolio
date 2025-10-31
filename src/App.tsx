1534cd39763df84c53b6c706d7219f481c59d49c
htu6n7yi <jcavalcanti008@gmail.com>
2025-10-31 16:49:30 -0300

    ajustes na aba projetos + melhorias no routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import { Banner } from "./components/banner/banner";
 import { NavigationMenuComponent } from "./components/banner/nav";
 import { CarouselSpacing } from "./components/banner/carousel";
-import { useState } from "react";
 import Projetos from "./pages/projetos";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
-  const [mainDefault, setMain] = useState(true);
 
-  // 1. Esta função será responsável por alterar o estado
-  const handleShowProjects = () => {
-    setMain(false); // Altera o estado para exibir a página de projetos
-  };
 
   return (
     <div className="bg-gradient-to-l from-slate-700 to-slate-900 flex flex-col justify-between min-h-screen">
